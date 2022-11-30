95031ca5327ee7dae6f74af0e944bf8bbd059f6a
Jack <UniformKitten281@gmail.com>
2022-11-30 13:07:15 -0800

    Update periodic.h

--- src/periodic.h
+++ src/periodic.h
@@ -4,9 +4,13 @@
 // Contains the periodic table data, functions, and different access methods.
 
 // Contains the elements of the periodic table
-extern char elements[118][MAX_STRING];
-char elementSymbols[118][MAX_STRING];
-double relativeAtomic[118];
+const char elements[118][MAX_STRING];
+const char elementSymbols[118][MAX_STRING];
+const double relativeAtomic[118];
+const int atomicNumbers[118];
+const char allElementGroups[118][MAX_STRING];
+const int elementsSpecificGroup[118];
+const int nobleGases[8];
 
 // GET functions
 void api_getElementName(char element[MAX_STRING]);
@@ -16,7 +20,7 @@ void api_getElementGroup(char element[MAX_STRING]);
 void api_getNGConfig(char element[MAX_STRING]);
 void api_getAtomicNumber(char element[MAX_STRING]);
 void api_getMolarMass(int element, int atom);
-void api_getElectronConfig(char element[MAX_STRING]);
+void api_getElectronConfig(int elementLine);
 
 // LIST functions
 void api_listAllElements();
